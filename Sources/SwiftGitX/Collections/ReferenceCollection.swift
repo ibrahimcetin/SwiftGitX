4fde60d3cb568f53f424d473a5a84106fb414cb1
İbrahim Çetin <cetinibrahim.ci@gmail.com>
2025-11-24 21:22:54 +0300

    Migrate ReferenceCollection

--- Sources/SwiftGitX/Collections/ReferenceCollection.swift
+++ Sources/SwiftGitX/Collections/ReferenceCollection.swift
@@ -1,8 +1,11 @@
-import libgit2
+//
+//  ReferenceCollection.swift
+//  SwiftGitX
+//
+//  Created by İbrahim Çetin on 24.11.2025.
+//
 
-public enum ReferenceCollectionError: Error {
-    case failedToList(String)
-}
+import libgit2
 
 /// A collection of references and their operations.
 public struct ReferenceCollection: Sequence {
@@ -10,14 +13,6 @@ public struct ReferenceCollection: Sequence {
 
     init(repositoryPointer: OpaquePointer) {
         self.repositoryPointer = repositoryPointer
-    }
-
-    // * I am not sure calling `git_error_last()` from a computed property is safe.
-    // * Because libgit2 docs say that "The error message is thread-local. The git_error_last() call must happen on the
-    // * same thread as the error in order to get the message."
-    // * But, I think it is worth a try.
-    private var errorMessage: String {
-        String(cString: git_error_last().pointee.message)
     }
 
     /// Retrieve a reference by its full name.
@@ -36,9 +31,7 @@ public struct ReferenceCollection: Sequence {
     ///   (e.g. `refs/heads/main`, `refs/tags/v1.0.0`,`refs/remotes/origin/main`)
     ///
     /// - Returns: The reference with the specified name.
-    ///
-    /// - Throws: A `ReferenceError` if an error occurs.
-    public func get(named fullName: String) throws -> (any Reference) {
+    public func get(named fullName: String) throws(SwiftGitXError) -> (any Reference) {
         let referencePointer = try ReferenceFactory.lookupReferencePointer(
             fullName: fullName,
             repositoryPointer: repositoryPointer
@@ -56,39 +49,34 @@ public struct ReferenceCollection: Sequence {
     /// - Returns: A list of references.
     ///
     /// The reference can be a `Branch`, a `Tag`.
-    ///
-    /// - Throws: A `ReferenceCollectionError.failedToList` if an error occurs.
-    public func list(glob: String? = nil) throws -> [any Reference] {
-        var referenceIterator: UnsafeMutablePointer<git_reference_iterator>?
-        defer { git_reference_iterator_free(referenceIterator) }
+    public func list(glob: String? = nil) throws(SwiftGitXError) -> [any Reference] {
+        var referenceIteratorPointer: UnsafeMutablePointer<git_reference_iterator>?
+        defer { git_reference_iterator_free(referenceIteratorPointer) }
 
-        let status =
+        try git(operation: .referenceList) {
             if let glob {
-                git_reference_iterator_glob_new(&referenceIterator, repositoryPointer, glob)
+                git_reference_iterator_glob_new(&referenceIteratorPointer, repositoryPointer, glob)
             } else {
-                git_reference_iterator_new(&referenceIterator, repositoryPointer)
+                git_reference_iterator_new(&referenceIteratorPointer, repositoryPointer)
             }
-
-        guard status == GIT_OK.rawValue else {
-            throw ReferenceCollectionError.failedToList(errorMessage)
         }
 
         var references = [any Reference]()
         while true {
-            var referencePointer: OpaquePointer?
-            defer { git_reference_free(referencePointer) }
+            do {
+                let referencePointer = try git(operation: .referenceList) {
+                    var referencePointer: OpaquePointer?
+                    let status = git_reference_next(&referencePointer, referenceIteratorPointer)
+                    return (referencePointer, status)
+                }
+                defer { git_reference_free(referencePointer) }
 
-            let nextStatus = git_reference_next(&referencePointer, referenceIterator)
-
-            if nextStatus == GIT_ITEROVER.rawValue {
-                break
-            } else if nextStatus != GIT_OK.rawValue {
-                throw ReferenceCollectionError.failedToList(errorMessage)
-            } else if let referencePointer {
                 let reference = try ReferenceFactory.makeReference(pointer: referencePointer)
                 references.append(reference)
-            } else {
-                throw ReferenceCollectionError.failedToList("Failed to get reference")
+            } catch  where error.code == .iterOver {
+                break
+            } catch {
+                throw error
             }
         }
 
@@ -98,4 +86,8 @@ public struct ReferenceCollection: Sequence {
     public func makeIterator() -> ReferenceIterator {
         ReferenceIterator(repositoryPointer: repositoryPointer)
     }
+}
+
+extension SwiftGitXError.Operation {
+    public static let referenceList = Self(rawValue: "referenceList")
 }
