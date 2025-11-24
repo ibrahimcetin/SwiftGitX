800531685ec6ccd5495f59962e5c165f77211a4a
İbrahim Çetin <cetinibrahim.ci@gmail.com>
2025-11-24 20:53:32 +0300

    Migrate status

--- Sources/SwiftGitX/Repository/Repository+status.swift
+++ Sources/SwiftGitX/Repository/Repository+status.swift
@@ -15,35 +15,27 @@ extension Repository {
     ///
     /// - Returns: The status of the repository.
     ///
-    /// - Throws: `RepositoryError.failedToGetStatus` if the status operation fails.
-    ///
     /// The status of the repository is represented by an array of `StatusEntry` values.
-    public func status(options optionFlags: StatusOption = .default) throws -> [StatusEntry] {
+    public func status(options optionFlags: StatusOption = .default) throws(SwiftGitXError) -> [StatusEntry] {
         // Initialize the status options
         var statusOptions = git_status_options()
-        let optionsInitStatus = git_status_options_init(&statusOptions, UInt32(GIT_STATUS_OPTIONS_VERSION))
-
-        guard optionsInitStatus == GIT_OK.rawValue else {
-            let errorMessage = String(cString: git_error_last().pointee.message)
-            throw RepositoryError.failedToGetStatus(errorMessage)
+        try git(operation: .status) {
+            git_status_options_init(&statusOptions, UInt32(GIT_STATUS_OPTIONS_VERSION))
         }
 
         // Set the status options
         statusOptions.flags = optionFlags.rawValue
 
         // Get the status list
-        var statusList: OpaquePointer?
-        defer { git_status_list_free(statusList) }
-
-        let statusListInitStatus = git_status_list_new(&statusList, pointer, &statusOptions)
-
-        guard statusListInitStatus == GIT_OK.rawValue else {
-            let errorMessage = String(cString: git_error_last().pointee.message)
-            throw RepositoryError.failedToGetStatus(errorMessage)
+        let statusListPointer = try git(operation: .status) {
+            var statusListPointer: OpaquePointer?
+            let statusListInitStatus = git_status_list_new(&statusListPointer, pointer, &statusOptions)
+            return (statusListPointer, statusListInitStatus)
         }
+        defer { git_status_list_free(statusListPointer) }
 
         // Get the status count
-        let statusCount = git_status_list_entrycount(statusList)
+        let statusCount = git_status_list_entrycount(statusListPointer)
 
         // Create an array to store the status entries
         var statusEntries: [StatusEntry] = []
@@ -51,11 +43,14 @@ extension Repository {
         // Iterate over the status entries
         for index in 0..<statusCount {
             // Get the status entry
-            let statusEntryPointer = git_status_byindex(statusList, index)
+            let statusEntryPointer = git_status_byindex(statusListPointer, index)
 
             // ? Should we handle the status entry differently if it's nil?
             guard let statusEntryPointer else {
-                throw RepositoryError.failedToGetStatus("Failed to get the status entry")
+                throw SwiftGitXError(
+                    code: .notFound, category: .invalid,
+                    message: "Failed to get the status entry at index \(index)"
+                )
             }
 
             // Create a StatusEntry instance from the status entry
@@ -74,8 +69,6 @@ extension Repository {
     ///
     /// - Returns: The status of the file.
     ///
-    /// - Throws: `RepositoryError.failedToGetStatus` if the status operation fails.
-    ///
     /// The path should be relative to the repository root directory. For example, `README.md` or
     /// `Sources/SwiftGitX/Repository.swift`.
     ///
@@ -83,14 +76,11 @@ extension Repository {
     /// Because a file can have multiple statuses. For example, a file can be both
     /// ``SwiftGitX/StatusEntry/Status-swift.enum/indexNew`` and
     /// ``SwiftGitX/StatusEntry/Status-swift.enum/workingTreeModified``.
-    public func status(path: String) throws -> [StatusEntry.Status] {
+    public func status(path: String) throws(SwiftGitXError) -> [StatusEntry.Status] {
         var statusFlags: UInt32 = 0
 
-        let status = git_status_file(&statusFlags, pointer, path)
-
-        guard status == GIT_OK.rawValue else {
-            let errorMessage = String(cString: git_error_last().pointee.message)
-            throw RepositoryError.failedToGetStatus(errorMessage)
+        try git(operation: .status) {
+            git_status_file(&statusFlags, pointer, path)
         }
 
         return StatusEntry.Status.from(statusFlags)
@@ -102,13 +92,11 @@ extension Repository {
     ///
     /// - Returns: The status of the file.
     ///
-    /// - Throws: `RepositoryError.failedToGetStatus` if the status operation fails.
-    ///
     /// The status of the file is represented by an array of `StatusEntry.Status` values.
     /// Because a file can have multiple statuses. For example, a file can be both
     /// ``SwiftGitX/StatusEntry/Status-swift.enum/indexNew`` and
     /// ``SwiftGitX/StatusEntry/Status-swift.enum/workingTreeModified``.
-    public func status(file: URL) throws -> [StatusEntry.Status] {
+    public func status(file: URL) throws(SwiftGitXError) -> [StatusEntry.Status] {
         let path = try file.relativePath(from: workingDirectory)
         return try status(path: path)
     }
