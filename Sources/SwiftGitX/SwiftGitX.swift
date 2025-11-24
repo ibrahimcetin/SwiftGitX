df03dd5b80c7b929617a1c187a86595ac976b655
İbrahim Çetin <cetinibrahim.ci@gmail.com>
2025-11-25 00:06:26 +0300

    Enhance error handling in SwiftGitX by adding operation context to error checks during initialization and shutdown. Introduce new operation constants for better clarity.

--- Sources/SwiftGitX/SwiftGitX.swift
+++ Sources/SwiftGitX/SwiftGitX.swift
@@ -18,11 +18,11 @@ public enum SwiftGitX {
     /// This function may be called multiple times. It will return the number of times the initialization has been
     /// called (including this one) that have not subsequently been shutdown.
     @discardableResult
-    public static func initialize() throws -> Int {
+    public static func initialize() throws(SwiftGitXError) -> Int {
         // Initialize the libgit2 library
         let status = git_libgit2_init()
 
-        try SwiftGitXError.check(status)
+        try SwiftGitXError.check(status, operation: .initialize)
 
         return Int(status)
     }
@@ -34,11 +34,11 @@ public enum SwiftGitX {
     /// Clean up the global state and threading context after calling it as many times as ``initialize()`` was called.
     /// It will return the number of remaining initializations that have not been shutdown (after this one).
     @discardableResult
-    public static func shutdown() throws -> Int {
+    public static func shutdown() throws(SwiftGitXError) -> Int {
         // Shutdown the libgit2 library
         let status = git_libgit2_shutdown()
 
-        try SwiftGitXError.check(status)
+        try SwiftGitXError.check(status, operation: .shutdown)
 
         return Int(status)
     }
@@ -53,4 +53,9 @@ public enum SwiftGitX {
 
         return "\(major).\(minor).\(patch)"
     }
+}
+
+extension SwiftGitXError.Operation {
+    public static let initialize = Self(rawValue: "initialize")
+    public static let shutdown = Self(rawValue: "shutdown")
 }
