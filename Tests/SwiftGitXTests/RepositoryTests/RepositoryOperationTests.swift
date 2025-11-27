6c393ba9e0b88b055cc477636e42f9234ede5636
İbrahim Çetin <cetinibrahim.ci@gmail.com>
2025-11-27 14:59:09 +0300

    Separate add tests

--- Tests/SwiftGitXTests/RepositoryTests/RepositoryOperationTests.swift
+++ Tests/SwiftGitXTests/RepositoryTests/RepositoryOperationTests.swift
@@ -9,26 +9,6 @@ import Foundation
 import SwiftGitX
 import Testing
 
-// MARK: - Add
-
-@Suite("Repository - Add", .tags(.repository, .operation, .add))
-final class RepositoryAddTests: SwiftGitXTest {
-    @Test("Add file to index")
-    func addFile() async throws {
-        let repository = mockRepository()
-
-        // Create a file
-        let file = try repository.mockFile()
-
-        // Add to index
-        try repository.add(file: file)
-
-        // Verify status
-        let status = try repository.status(file: file)
-        #expect(status == [.indexNew])
-    }
-}
-
 // MARK: - Commit
 
 @Suite("Repository - Commit", .tags(.repository, .operation, .commit))
