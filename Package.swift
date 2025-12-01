a37ee3fd47a63895f408a12ac2e051d98eedfde8
İbrahim Çetin <cetinibrahim.ci@gmail.com>
2025-12-01 12:10:53 +0300

    Update libgit2 dependency to version 1.9.2 with exact versioning in Package.swift and refresh Package.resolved to reflect the new revision.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ibrahimcetin/libgit2.git", branch: "v1.9.2")
+        .package(url: "https://github.com/ibrahimcetin/libgit2.git", exact: "1.9.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
